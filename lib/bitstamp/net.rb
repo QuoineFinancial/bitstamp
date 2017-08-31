92be92c0180d41e1ee407c8efcbd815035f83843
Cuong Vu <cuongvu0103@gmail.com>
2017-08-31 11:28:59 +0700

    Use milisecond for nonce

--- lib/bitstamp/net.rb
+++ lib/bitstamp/net.rb
@@ -23,8 +23,8 @@ module Bitstamp
     def self.bitstamp_options(options={})
       if Bitstamp.configured?
         options[:key] = Bitstamp.key
-        options[:nonce] = (Time.now.to_f*10000).to_i.to_s
-        options[:signature] = HMAC::SHA256.hexdigest(Bitstamp.secret, options[:nonce]+Bitstamp.client_id.to_s+options[:key]).upcase
+        options[:nonce] = (Time.now.to_f * 1_000_000_000).to_i.to_s
+        options[:signature] = HMAC::SHA256.hexdigest(Bitstamp.secret, options[:nonce] + Bitstamp.client_id.to_s + options[:key]).upcase
       end
 
       options
