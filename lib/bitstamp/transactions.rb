8286b3f68535d18c04e27c56259e441bbc3f7ad6
Unreal Hoang <unrealhoang@gmail.com>
2018-02-08 18:58:49 +0900

    support bch for bitstamp

--- lib/bitstamp/transactions.rb
+++ lib/bitstamp/transactions.rb
@@ -22,7 +22,7 @@ module Bitstamp
   end
 
   class UserTransaction < Bitstamp::Model
-    attr_accessor :datetime, :id, :type, :usd, :btc, :fee, :order_id, :btc_usd, :nonce, :eth_btc, :eur, :eth, :btc_eur, :eth_eur, :eth_usd
+    attr_accessor :datetime, :id, :type, :usd, :btc, :fee, :order_id, :btc_usd, :nonce, :eth_btc, :eur, :eth, :btc_eur, :eth_eur, :eth_usd, :bch_usd, :bch
   end
 
   # adding in methods to pull the last public trades list
